50423239311e361bad30a6239f16ffa07d7f59a8
Langwenchong <1422257646@qq.com>
2022-02-16 20:31:09 +0800

    first commit

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="cachedViews">
+        <!-- 新增  -->
+        <router-view :key="key" />
+      </keep-alive>
+      <!-- 新增 -->
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+  computed: {
+    key() {
+      return this.$route.path;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.app-main {
+  /*50 = navbar  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.fixed-header + .app-main {
+  padding-top: 50px;
+}
+</style>
+
+<style lang="scss">
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
+</style>
